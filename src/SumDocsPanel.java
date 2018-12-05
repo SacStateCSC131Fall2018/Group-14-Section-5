88aa02652ef899f67c63f0a1ef07eff144f428df
Jordan <Jordan@10.0.0.68>
2018-12-04 16:54:48 -0800

    Added Filler Text to SumDocsPanel

--- src/SumDocsPanel.java
+++ src/SumDocsPanel.java
@@ -23,7 +23,19 @@ public class SumDocsPanel extends JPanel {
 		
 		// text area
 		textArea.setLineWrap(true);
-		textArea.setText("Opus 0: get smoked");
+		textArea.setText("Opus 0: Charles Dickens  Bleak House  7312 documents\n");
+		textArea.append("               /Users/christopherfox/Programs/java/ir/data/bleakhouse.txt\n");
+		textArea.append("Opus 1: Thomas Hardy  Far from the Madding Crowd  3538 documents\n");
+		textArea.append("               /Users/christopherfox/Programs/java/ir/data/farFromTheMaddingCrowd.txt\n");
+		textArea.append("Opus 2: Thomas Hardy  Jude the Obscure  3650\n");
+		textArea.append("               /Users/christopherfox/Programs/java/ir/data/judeTheObscure.txt\n");
+		textArea.append("Opus 3: Daniel Defoe  The Fortunes and Misfortunes of the Famous Moll Flanders &c  1283 documents\n");
+		textArea.append("               /Users/christopherfox/Programs/java/ir/data/mollFlanders.txt\n");
+		textArea.append("Opus 4: Charles Dickens  Great Expectations  3848 documents\n");
+		textArea.append("               /Users/christopherfox/Programs/java/ir/data/greatExpectations.txt\n");
+		textArea.append("Opus 5: Charles Dickens  A Tale of Two Cities  3329 documents\n");
+		textArea.append("               /Users/christopherfox/Programs/java/ir/data/taleOfTwoCities.txt\n");
+		textArea.append("\n\nIndex Terms: 26447\nPostings:     226412");
 		
 		// scroll bar
 		textAreaPanel.add(new JScrollPane(textArea,ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS,ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER));
